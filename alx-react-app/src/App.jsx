7ce624c4396006c90cdeaa5494fa0a067f691b36
ElleWest <izelle.vanderwesthuizen@iu-study.org>
2025-11-02 17:10:50 +0200

    Use single quotes for component imports

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import WelcomeMessage from "./components/WelcomeMessage";
 import UserProfile from "./components/UserProfile";
-import Header from "./Header";
-import MainContent from "./MainContent";
-import Footer from "./Footer";
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   return (
